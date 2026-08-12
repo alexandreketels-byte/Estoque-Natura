0b294eec151e02ec10172831ffbd8bcd9f26e720
alexandreketels-byte <alexandreketels@gmail.com>
2026-08-12 16:46:20 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Cole aqui a URL da implantação (Web App) do seu Apps Script,
 // disponível em: Extensões > Apps Script > Implantar > Gerenciar implantações
-const API_URL = 'COLE_AQUI_A_URL_DO_APPS_SCRIPT';
+const API_URL = 'https://script.google.com/macros/s/AKfycby4Yi24mDTqT8xT-kDMS0BYFab4BDPs0T9m1b9Oz1U45GT4vGimAYVb4WTGeldb4qAe/exec';
